a29261e63408c1e61b983d9b7b8e5070c86556d3
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-03 13:00:12 +0800

    Update addition_B.m

--- ECC_B/addition_B.m
+++ ECC_B/addition_B.m
@@ -2,8 +2,9 @@
 % Addition over B
 %
 function out = addition_B(a, b)
-a = uint32(a);
-b = uint32(b);
-c = bitxor(a, b);
-out = double(c);
+    a = uint32(a);
+    b = uint32(b);
+    c = bitxor(a, b);
+    out = double(c);
 return
+
