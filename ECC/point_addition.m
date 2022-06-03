3d07cc686e6bc63a4218985a35d7539c9f0f638c
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-03 12:57:43 +0800

    Update point_addition.m

--- ECC/point_addition.m
+++ ECC/point_addition.m
@@ -3,20 +3,19 @@
 %
 function R = point_addition(P, Q)
     global p;
-    
     if P(1) == Q(1) && P(2) == Q(2)
         % if P == Q
         R = point_multiplication(P, 2);
     else
         % if P != Q
         for ip = 1 : p-1
-            if mod((Q(1)-P(1))*ip, p) == 1
+            if mod((Q(1) - P(1))*ip, p) == 1
                 di = ip;
             end
         end
-        
         delta = mod((Q(2) - P(2))*di, p);
         temp  = P(1);
+        
         Rx = mod(delta^2 - P(1) - Q(1), p);
         Ry = mod(delta*(temp - Rx) - P(2), p);
         R = [Rx Ry];
