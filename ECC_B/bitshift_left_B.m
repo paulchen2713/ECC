36eba897047562039f8fa5746e1e179a806c1b81
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-04 15:25:11 +0800

    Add files via upload

---
+++ ECC_B/bitshift_left_B.m
@@ -0,0 +1,15 @@
+%
+% bit left-shift by one
+%
+function out=bitshift_left_B(a, param)
+word_num = size(param.S, 1);
+a=uint32(a);
+bit_old=0;
+for i=1:word_num
+    bit=bitget(a(i),32);
+    a(i)=bitshift(a(i),1);
+    a(i)=bitset(a(i),1,bit_old);
+    bit_old=bit;    
+end
+out=double(a);
+return
