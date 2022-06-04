024ac086346dbf902f010d228d896c265f14fa18
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-04 15:28:37 +0800

    Update bitshift_right_B.m

--- ECC_B/bitshift_right_B.m
+++ ECC_B/bitshift_right_B.m
@@ -1,14 +1,15 @@
 %
 % bit left-shift by one
 %
-function out=bitshift_right_B(a, param)
-word_num = size(param.S, 1);
-bit_old=0;
-for i=1:word_num
-    bit=bitget(a(word_num-i+1),1);
-    a(word_num-i+1)=bitshift(a(word_num-i+1),-1);
-    a(word_num-i+1)=bitset(a(word_num-i+1),32,bit_old);
-    bit_old=bit;    
-end
-out=a;
+function out = bitshift_right_B(a, param)
+    word_num = size(param.S, 1);
+    bit_old=0;
+    for i=1:word_num
+        bit=bitget(a(word_num-i+1),1);
+        a(word_num-i+1)=bitshift(a(word_num-i+1),-1);
+        a(word_num-i+1)=bitset(a(word_num-i+1),32,bit_old);
+        bit_old=bit;    
+    end
+    out=a;
 return
+
