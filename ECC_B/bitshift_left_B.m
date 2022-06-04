f2457a2d87cb9042eb9826ba06f013330f43abb7
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-04 15:26:56 +0800

    Update bitshift_left_B.m

--- ECC_B/bitshift_left_B.m
+++ ECC_B/bitshift_left_B.m
@@ -3,13 +3,13 @@
 %
 function out = bitshift_left_B(a, param)
     word_num = size(param.S, 1);
-    a=uint32(a);
-    bit_old=0;
-    for i=1:word_num
-        bit=bitget(a(i),32);
-        a(i)=bitshift(a(i),1);
-        a(i)=bitset(a(i),1,bit_old);
-        bit_old=bit;    
+    a = uint32(a);
+    bit_old = 0;
+    for i = 1:word_num
+        bit = bitget(a(i), 32);
+        a(i) = bitshift(a(i), 1);
+        a(i) = bitset(a(i), 1, bit_old);
+        bit_old = bit;    
     end
-    out=double(a);
+    out = double(a);
 return
