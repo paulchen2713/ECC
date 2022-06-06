bf2e9fd72149b830f5941a3bd4276d87c9fbad9a
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-06 11:11:15 +0800

    Update ECC_B_series.m

--- ECC_B/ECC_B_series.m
+++ ECC_B/ECC_B_series.m
@@ -1,7 +1,9 @@
+%
 % ECC B series ElGamal crytographic systems
+%
 clear; 
 clc;
-
+%
 series = 'B-233'; % 163, 233, 283, 409, 571
 param = produce_ECC_param(series);
 dec_param = ECC_B_param2dec(param);
@@ -9,13 +11,13 @@ word_num = size(dec_param.S, 1);
 zero = zeros(word_num, 1);
 one = zeros(word_num, 1);
 one(1) = 1;
-
+%
 % polynomial test
 % ref: Guide, p.81
 left = addition_B(square_B(dec_param.y, dec_param), multiplication_B(dec_param.x, dec_param.y, dec_param));
 right = addition_B(multiplication_B(square_B(dec_param.x, dec_param), addition_B(dec_param.x, dec_param.a), dec_param), dec_param.b);
 check = any(left - right);
-
+%
 % ECC cryptographic system
 % A sends message to B
 %
@@ -25,7 +27,7 @@ check = any(left - right);
 op_time = cputime; % get cpu time
 nb = rand_col_vector(dec_param, dec_param.n);
 [Pbx, Pby] = point_multiplication_B(dec_param.x, dec_param.y, nb, dec_param);
-
+%
 % encryption
 % message: M
 % private key: na
@@ -35,9 +37,9 @@ M_len = length(M);
 M_dec = double(M);
 Pmx = zeros(word_num, M_len);
 Pmy = zeros(word_num, M_len);
-
+%
 load('M');
-
+%
 for im = 1: M_len
     Pmx(:, im) = Mx(:, M_dec(im) - 31);
     Pmy(:, im) = My(:, M_dec(im) - 31);
@@ -59,7 +61,6 @@ for im = 1: M_len
     PmnaPbx(:, im) = multiplication_B(PmnaPbx(:, im), Z2, dec_param);
     PmnaPby(:, im) = multiplication_B(PmnaPby(:, im), Z3, dec_param);
 end
-
 %
 % decryption
 % recerved message: Pa, PmnaPb
@@ -86,10 +87,9 @@ for im = 1: M_len
     R_M_dec(im) = floor(R_Pmx(1, im) / 100);
 end
 R_M = char(R_M_dec);
-
+%
 fprintf('the original message: %s\n', M);
 fprintf('the recovery message: %s\n', R_M);
 op_time = cputime - op_time;
 fprintf('time = %d\n', op_time);
-
-
+%
