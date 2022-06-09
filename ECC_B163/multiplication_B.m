e225a2e5bdbaffc54761ccdca43d014a3c3e1dc9
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-09 11:06:38 +0800

    Update multiplication_B.m

--- ECC_B163/multiplication_B.m
+++ ECC_B163/multiplication_B.m
@@ -5,25 +5,26 @@
 %
 % Algorithm 2.34 Right-to-left comb method for polynomial multiplication
 function out = multiplication_B(a, b)
-% global ifx;
-%
-f = ['000000c9';'00000000';'00000000';'00000000';'00000000';'00000008'];
-%
-ifx = size(f, 1);
-%
-b(ifx + 1) = 0;
-c = zeros(2*ifx, 1); % 12 sections of 
-for k = 1 : 32
-    for j = 1 : ifx
-        % if the bit == 1(exist) then perform operation
-        if bitget(a(j), k) == 1
-            Cj = c(j : j+ifx);
-            Cj = addition_B(Cj, b);
-            c(j : j+ifx) = Cj;
+    % global ifx;
+    %
+    f = ['000000c9';'00000000';'00000000';'00000000';'00000000';'00000008'];
+    %
+    ifx = size(f, 1);
+    %
+    b(ifx + 1) = 0;
+    c = zeros(2*ifx, 1); % 12 sections of 
+    for k = 1 : 32
+        for j = 1 : ifx
+            % if the bit == 1(exist) then perform operation
+            if bitget(a(j), k) == 1
+                Cj = c(j : j+ifx);
+                Cj = addition_B(Cj, b);
+                c(j : j+ifx) = Cj;
+            end
         end
+        % special case for 
+        b = special_bitshift_left_B(b);
     end
-    % special case for 
-    b = special_bitshift_left_B(b);
-end
-out = reduction_B163(c);
+    out = reduction_B163(c);
 return
+
