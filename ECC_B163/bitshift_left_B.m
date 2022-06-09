577d719ea42b1e645e3de431c21e23384b0ba636
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-09 11:05:08 +0800

    Update bitshift_left_B.m

--- ECC_B163/bitshift_left_B.m
+++ ECC_B163/bitshift_left_B.m
@@ -2,19 +2,20 @@
 % bit left-shift by one function
 %
 function out = bitshift_left_B(a)
-% global ifx;
-%
-f = ['000000c9';'00000000';'00000000';'00000000';'00000000';'00000008'];
-%
-ifx = size(f, 1);
-%
-a = uint32(a);
-bit_old = 0; % temp for msb in each row
-for i = 1 : ifx
-    bit = bitget(a(i), 32);
-    a(i) = bitshift(a(i), 1);
-    a(i) = bitset(a(i), 1, bit_old);
-    bit_old = bit;    
-end
-out = double(a);
+    % global ifx;
+    %
+    f = ['000000c9';'00000000';'00000000';'00000000';'00000000';'00000008'];
+    %
+    ifx = size(f, 1);
+    %
+    a = uint32(a);
+    bit_old = 0; % temp for msb in each row
+    for i = 1 : ifx
+        bit = bitget(a(i), 32);
+        a(i) = bitshift(a(i), 1);
+        a(i) = bitset(a(i), 1, bit_old);
+        bit_old = bit;    
+    end
+    out = double(a);
 return
+
